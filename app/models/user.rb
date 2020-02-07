66f761a333629a00f89dd34338b42bd8173fcb92
Andy Smith <andjsmit@iupui.edu>
2020-02-07 14:18:57 -0500

    Complete CAS authentication. Add Rivet styling.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :omniauthable, omniauth_providers: [:cas]
+
+  def self.from_iucas(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user
+    end
+  end
 end
