25c008948b243260a2ac04bc4ae13adb3d618180
Andy Smith <andjsmit@iupui.edu>
2020-05-29 16:15:07 -0400

    Make all (except other topics) fields required in covid form.

--- app/models/covid_photo.rb
+++ app/models/covid_photo.rb
@@ -1,4 +1,6 @@
 class CovidPhoto < ApplicationRecord
   has_and_belongs_to_many :topics
   has_one_attached :photo
+
+  validates :date_taken, :location, :author, :information, :topics, :photo, :submit_name, :submit_email, presence: true
 end
