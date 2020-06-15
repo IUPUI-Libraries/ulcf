16bc3c4d7e327bdc53b0ba7bea04fbc225d69da4
Andy Smith <andjsmit@iupui.edu>
2020-06-15 15:00:53 -0400

    Validate other topic text field when other topic is checked.

--- app/models/covid_photo.rb
+++ app/models/covid_photo.rb
@@ -2,5 +2,15 @@ class CovidPhoto < ApplicationRecord
   has_and_belongs_to_many :topics
   has_one_attached :photo
 
-  validates :date_taken, :location, :author_first, :author_last, :information, :topics, :photo, :submit_name, :submit_email, presence: true
+  validates :date_taken, :location, :author_first, :author_last, :information,
+            :topics, :photo, :submit_name, :submit_email, presence: true
+  validates_presence_of :other_topic, if: :other_checked?
+
+  def other_checked?
+    topics.each do |t|
+      return true if t.name == 'Other'
+    end
+
+    false
+  end
 end
